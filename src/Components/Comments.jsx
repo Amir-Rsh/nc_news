1030ec6832475f47dd15430007307bb40a05da44
Amir-Rsh <amirrashidinia93@gmail.com>
2024-02-07 09:17:19 +0000

    empty comments message added

--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -13,23 +13,35 @@ export default function Comments({ article_id }) {
       <h3 className="header" style={{ paddingTop: "3%" }}>
         Comments:
       </h3>
-      {commentList.map((comment) => {
-        return (
-          <div
-            className="card w-70 mb-3"
-            style={{ width: "70%", margin: "auto" }}
-            key={comment.comment_id}
-          >
-            <div className="card-body">
-              <h5 className="card-title">{comment.author}:</h5>
-              <p className="card-text">{comment.body}</p>
-              <p className="card-text" style={{ textAlign: "right" }}>
-                votes: {comment.votes}
-              </p>
+      {commentList.length === 0 ? (
+        <h5
+          style={{
+            textAlign: "center",
+            paddingTop: "4%",
+            paddingBottom: "12%",
+          }}
+        >
+          this article has no comments
+        </h5>
+      ) : (
+        commentList.map((comment) => {
+          return (
+            <div
+              className="card w-70 mb-3"
+              style={{ width: "70%", margin: "auto" }}
+              key={comment.comment_id}
+            >
+              <div className="card-body">
+                <h5 className="card-title">{comment.author}:</h5>
+                <p className="card-text">{comment.body}</p>
+                <p className="card-text" style={{ textAlign: "right" }}>
+                  votes: {comment.votes}
+                </p>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </>
   );
 }
