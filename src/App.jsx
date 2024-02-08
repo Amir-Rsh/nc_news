6641dc1b007c4e043cce69085c0a1c4c89c2e8f6
Amir-Rsh <amirrashidinia93@gmail.com>
2024-02-08 13:57:09 +0000

    fixed state for article list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Nav from "./Components/Nav";
 import { Routes, Route } from "react-router-dom";
 import ListOfArticles from "./Components/ListOfArticles";
-import getArticles from "../api";
 import ArticlePage from "./Components/ArticlePage";
 import UserContext from "./Contexts/UserContext";
 import Users from "./Components/Users";
 
 function App() {
-  const [articles, setArticles] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
   const [loggedInUser, setLoggedInUser] = useState({
     username: "grumpy19",
     name: "Paul Grump",
@@ -17,23 +14,15 @@ function App() {
       "https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013",
   });
 
-  useEffect(() => {
-    getArticles().then(({ data: { articles } }) => {
-      setArticles(articles);
-      setIsLoading(false);
-    });
-  }, []);
-
   return (
     <>
       <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
         <Nav />
         <Routes>
-          <Route path="/" element={<ListOfArticles articles={articles} />} />
+          <Route path="/" element={<ListOfArticles />} />
           <Route path="/articles/:article_id" element={<ArticlePage />} />
           <Route path="/users" element={<Users />} />
         </Routes>
-        {isLoading ? <h5>Loading the articles</h5> : null}
       </UserContext.Provider>
     </>
   );
--- src/Components/ListOfArticles.jsx
+++ src/Components/ListOfArticles.jsx
@@ -1,10 +1,22 @@
+import getArticles from "../../api";
 import ArticleCard from "./ArticleCard";
 import HomeHeader from "./HomeHeader";
+import { useEffect, useState } from "react";
 
-export default function ListOfArticles({ articles }) {
+export default function ListOfArticles() {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    getArticles().then(({ data: { articles } }) => {
+      setArticles(articles);
+      setIsLoading(false);
+    });
+  }, []);
   return (
     <>
       <HomeHeader />
+      {isLoading ? <h6>Loading the articles</h6> : null}
+
       <section>
         <div className="container text-center">
           <div className="row" style={{ paddingBottom: "2%" }}>
