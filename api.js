3815be8f277f50a95cddd764eae723260445a991
Amir-Rsh <amirrashidinia93@gmail.com>
2024-03-25 03:54:17 +0000

    styling and firebase auth

--- api.js
+++ api.js
@@ -42,3 +42,51 @@ export function deleteComment(id) {
     `https://nc-news-by-amir.onrender.com/api/comments/${id}`
   );
 }
+
+export function postUser(uid, username, name, avatar_url, navigate) {
+  return axios
+    .post(`https://nc-news-by-amir.onrender.com/api/users`, {
+      user_id: uid,
+      username: username,
+      name: name,
+      avatar_url: avatar_url,
+    })
+    .then(() => {
+      navigate("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+export function getUserByUserId(user_id, setLoggedInUser) {
+  return axios
+    .get(`https://nc-news-by-amir.onrender.com/api/users/${user_id}`)
+    .then((response) => {
+      setLoggedInUser(response.data.user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+export function checkUsername(
+  username,
+  setUserError,
+  setUserChecker,
+  setUserConfirmed
+) {
+  return axios
+    .post(`https://nc-news-by-amir.onrender.com/api/users`, {
+      username: username,
+    })
+    .then(() => {})
+    .catch((err) => {
+      setUserChecker(false);
+      if (err.response.data.msg === "username already exists") {
+        setUserError(true);
+      } else {
+        setUserConfirmed(true);
+      }
+    });
+}
--- src/Components/HomeHeader.jsx
+++ src/Components/HomeHeader.jsx
@@ -1,7 +1,7 @@
 export default function HomeHeader() {
   return (
     <header style={{ paddingTop: "5em" }}>
-      <h1 className="header">NC News Homepage</h1>
+      <h1 className="header">NC News Articles</h1>
     </header>
   );
 }
